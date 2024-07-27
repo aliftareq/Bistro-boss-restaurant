7b9a08d28def38a71c4360466b7697362e5b1274
alif <aliftareq@gmail.com>
2024-07-27 10:27:48 +0600

    made custom hook useMenu

--- bistro-boss-client/src/pages/MenuPage/Menu/Menu.jsx
+++ bistro-boss-client/src/pages/MenuPage/Menu/Menu.jsx
@@ -1,7 +1,6 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../../shared/Cover/Cover";
 import menuImg from "../../../assets/menu/banner3.jpg";
-import PopulerMenu from "../../Homepage/PopulerMenu/PopulerMenu";
 
 const Menu = () => {
   return (
@@ -10,11 +9,6 @@ const Menu = () => {
         <title>Bistro Boss| Menu</title>
       </Helmet>
       <Cover img={menuImg} title="Our Menu"></Cover>
-      <PopulerMenu></PopulerMenu>
-      <Cover img={menuImg} title="Our Menu"></Cover>
-      <PopulerMenu></PopulerMenu>
-      <Cover img={menuImg} title="Our Menu"></Cover>
-      <PopulerMenu></PopulerMenu>
     </div>
   );
 };
