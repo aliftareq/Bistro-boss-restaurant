f3bed51ce4aa7c5ee135b90df981ee9dada5efc9
alif <aliftareq@gmail.com>
2024-07-27 10:11:03 +0600

    cover component with parallax created

--- bistro-boss-client/src/pages/MenuPage/Menu/Menu.jsx
+++ bistro-boss-client/src/pages/MenuPage/Menu/Menu.jsx
@@ -1,4 +1,7 @@
 import { Helmet } from "react-helmet-async";
+import Cover from "../../shared/Cover/Cover";
+import menuImg from "../../../assets/menu/banner3.jpg";
+import PopulerMenu from "../../Homepage/PopulerMenu/PopulerMenu";
 
 const Menu = () => {
   return (
@@ -6,7 +9,12 @@ const Menu = () => {
       <Helmet>
         <title>Bistro Boss| Menu</title>
       </Helmet>
-      <h2>Menu page</h2>
+      <Cover img={menuImg} title="Our Menu"></Cover>
+      <PopulerMenu></PopulerMenu>
+      <Cover img={menuImg} title="Our Menu"></Cover>
+      <PopulerMenu></PopulerMenu>
+      <Cover img={menuImg} title="Our Menu"></Cover>
+      <PopulerMenu></PopulerMenu>
     </div>
   );
 };
