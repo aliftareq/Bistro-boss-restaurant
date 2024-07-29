caa208c7be7b556fa9e12667f1a99177a27e1043
alif <aliftareq@gmail.com>
2024-07-29 06:59:27 +0600

    Login form created

--- bistro-boss-client/src/pages/shared/NavBar/NavBar.jsx
+++ bistro-boss-client/src/pages/shared/NavBar/NavBar.jsx
@@ -47,6 +47,9 @@ const NavBar = () => {
             <li>
               <Link to="/order/salad">Order Food</Link>{" "}
             </li>
+            <li>
+              <Link to="/login">Login</Link>{" "}
+            </li>
           </ul>
         </div>
         <div className="navbar-end">
